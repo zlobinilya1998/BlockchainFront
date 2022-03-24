514b0b5d45817f424551157f785ef07f6c1ffb0e
zlobin_ilya <qwertin1998@gmail.com>
2022-03-25 02:31:35 +0300

    Add vue router

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,30 @@
 <template>
-    <div class="app">
-        123
-    </div>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
 
-<script>
-export default {
-
-}
-</script>
-
 <style>
-body {
-    margin: 0 !important;
-    background: #000000;
-    color: white;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
-
