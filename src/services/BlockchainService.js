fa82105be7eb27b2ea95e1c2910e479688fac2ef
zlobin_ilya <qwertin1998@gmail.com>
2022-03-25 00:39:12 +0300

    Add vercel path

--- src/services/BlockchainService.js
+++ src/services/BlockchainService.js
@@ -1,7 +1,7 @@
 import {DefaultResult} from "@/helpers";
 
 export default class BlockchainService {
-    static apiPath = 'http://localhost:5000/api';
+    static apiPath = 'https://blockchain-backend.vercel.app/api';
     static baseUrl = '/bc';
     static async getSymbols(){
        const res = await fetch(this.apiPath + this.baseUrl + '/symbols');
